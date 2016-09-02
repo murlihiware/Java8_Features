e31895115109ffd551ff20c93806cc27e0c402e4
Hiware, Murlidhar <Murlidhar.Hiware@fisglobal.com>
2016-09-02 17:02:06 +0530

    Stream example 1

--- src/com/streams/StreamOperations.java
+++ src/com/streams/StreamOperations.java
@@ -3,6 +3,11 @@
  */
 package com.streams;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author Murlidhar.Hiware
  *
@@ -13,8 +18,88 @@ public class StreamOperations {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		List<Transaction> transactions = new ArrayList<Transaction>();
 
 	}
+	
+	public static Transaction getMaxGroceryTransaction(List<Transaction> transactions)
+	{
+		List<Transaction> groceryTransactions = new ArrayList<>();
+		for(Transaction t: transactions){
+		  if(t.getType() == Transaction.TYPE_GROCERY){
+		    groceryTransactions.add(t);
+		  }
+		}
+		
+		Collections.sort(groceryTransactions, new Comparator<Transaction>(){
+			
+			@Override
+			public int compare(Transaction t1, Transaction t2) {
+				return Integer.compare(t1.getValue(), t2.getValue());
+			}
+			});
+		return groceryTransactions.get(0);
+	}
+	
+	public static Transaction getMaxGroceryTransactionJava8(List<Transaction> transactions)
+	{
+		//transactions.stream().filter(t -> t.getType()==Transaction.TYPE_GROCERY).sorted().collect();
+	    return null;
+	}
+	
+	
+	
+	
+	
+	public static List<Transaction> createTransactions()
+ {
+		List<Transaction> transactions = new ArrayList<Transaction>();
+		transactions.add(new Transaction(Transaction.TYPE_GROCERY, 1000));
+		transactions.add(new Transaction(Transaction.TYPE_GROCERY, 500));
+		transactions.add(new Transaction(Transaction.TYPE_GROCERY, 200));
+		transactions.add(new Transaction(Transaction.TYPE_GROCERY, 4500));
+		transactions.add(new Transaction(Transaction.TYPE_GROCERY, 9500));
+		transactions.add(new Transaction(Transaction.TYPE_ELECTRONICS, 10500));
+		transactions.add(new Transaction(Transaction.TYPE_ELECTRONICS, 12500));
+		transactions.add(new Transaction(Transaction.TYPE_ELECTRONICS, 14500));
+		transactions.add(new Transaction(Transaction.TYPE_ELECTRONICS, 24500));
+		transactions.add(new Transaction(Transaction.TYPE_BOOKS, 500));
+		transactions.add(new Transaction(Transaction.TYPE_BOOKS, 100));
+		transactions.add(new Transaction(Transaction.TYPE_BOOKS, 3100));
+		transactions.add(new Transaction(Transaction.TYPE_BOOKS, 1200));
+		return transactions;
+	}
 
+}
+
+
+class Transaction
+{
+	public static final int TYPE_GROCERY = 1;
+	public static final int TYPE_ELECTRONICS = 2;
+	public static final int TYPE_BOOKS = 3;
+	private int type;
+	private int value;
+	
+	
+	
+	public Transaction(int type, int value) {
+		this.type = type;
+		this.value = value;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
 }
